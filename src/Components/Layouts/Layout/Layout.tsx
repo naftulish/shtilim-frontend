b8af966edfb9156ee173c374afba6811dc68e37a
Zvimorgen <zvimorgen@gmail.com>
2023-05-24 15:47:29 +0300

    update layout

--- src/Components/Layouts/Layout/Layout.tsx
+++ src/Components/Layouts/Layout/Layout.tsx
@@ -1,47 +1,154 @@
-import React, { useState } from 'react';
-import './Layout.css';
-import Main from '../Main/Main';
-import Aside from '../Aside/Aside';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import GroupsIcon from '@mui/icons-material/Groups';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
-export function Layout() {
-    const [isExpanded, setIsExpanded] = useState(false);
+const drawerWidth = 240;
 
-    const toggleDrawer = () => {
-        setIsExpanded(!isExpanded);
-    };
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen
+  }),
+  overflowX: "hidden"
+});
 
-    const list = (
-        <Box sx={{ width: 'auto', marginTop: '50px' }}>
-            <List>
-                {['סטודנטים'].map((text, index) => (
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open"
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginRight: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open"
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme)
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme)
+  })
+}));
+
+export  function Layout() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div dir="rtl">
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+            //    edge="start"
+              sx={{
+                marginRight:0,
+                ...(open && { display: "none" })
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div" style= {{color: 'black'}}>
+             תפריט
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open} anchor="right">
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+
+         <Box sx={{ width: '20px', marginTop: '20px' }}>
+             <List>
+                 {['תלמידים', 'תוכניות', 'הוספת תלמידים', 'הגדרות'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            {index  === 0 ? <GroupsIcon /> :  <></>}
+                            {index  === 1 ? <AssignmentIcon /> :  <></>}
+                            {index  === 2 ? <PersonAddIcon /> :  <></>}
+                            {index  === 3 ? <SettingsIcon />: <></>}
                             </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItemButton>
@@ -49,34 +156,20 @@ export function Layout() {
                 ))}
             </List>
         </Box>
-    );
-
-    return (
-        <div>
-            <div style={{ height: '50px', width: '100vw', backgroundColor: 'blue', position: 'fixed', top: 0 }}>
-                <Button style={{ color: 'red' }} onClick={toggleDrawer}>
-                    {isExpanded ? 'Close Drawer' : 'Open Drawer'}
-                </Button>
-            </div>
-
-            <Drawer
-                anchor="right"
-                open={isExpanded}
-                onClose={toggleDrawer}
-                variant="permanent"
-                sx={{
-                    '& .MuiDrawer-paper': {
-                        width: isExpanded ? 240 : 60,
-                        boxSizing: 'border-box',
-                        transition: 'width 0.2s ease-in-out',
-                        marginTop: '50px', // Adjust the margin top as per your needs
-                    },
-                }}
-            >
-                {list}
-            </Drawer>
-        </div>
-    );
+        
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography paragraph>
+            
+          </Typography>
+          <Typography paragraph>
+           
+          </Typography>
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
 export default Layout;

487f158f8c80ab1281f1ce98f0b2b86dbd8b2291
MeirShuvax <124002947+MeirShuvax@users.noreply.github.com>
2023-05-24 12:34:45 +0300

    ubdate interface

--- src/Components/Layouts/Layout/Layout.tsx
+++ src/Components/Layouts/Layout/Layout.tsx
@@ -14,7 +14,41 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { createTheme } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
 
+
+
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+const theme = createTheme({
+  direction: 'rtl',
+});
+
+
+function RTL(props) {
+  return <CacheProvider value={cacheRtl}>{props.children}</CacheProvider>;
+}
+
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset } from '@mui/styles';
+
+// Configure JSS
+const jss = create({
+  plugins: [...jssPreset().plugins, rtl()],
+});
+
+function RTL(props) {
+  return <StylesProvider jss={jss}>{props.children}</StylesProvider>;
+}
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 export function Layout() {
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './Components/Layouts/Layout/Layout';
 import { BrowserRouter } from 'react-router-dom';
+import { Dialog } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout />
+    {/* <Dialog dir="rtl">  */}
+    <Layout />
+{/* </Dialog> */}
     </BrowserRouter>
   </React.StrictMode>
 );

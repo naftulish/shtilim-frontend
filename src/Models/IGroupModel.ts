293ac6e93a72b568fa87c0726df97d4491ac36c9
Eli <elisadbig90@gmail.com>
2023-08-03 17:14:56 +0300

    last V

--- src/Models/IGroupModel.ts
+++ src/Models/IGroupModel.ts
@@ -5,3 +5,4 @@ interface Group{
     
 }
 export default Group;
+
--- src/Models/IStudentModel.ts
+++ src/Models/IStudentModel.ts
@@ -6,8 +6,6 @@ interface IStudentModel{
     dateOfBirth: Date,
     gender: string,
     address: string,
-    // readonly createdIn?: Date;
-    // createdIn: Date // not in frontend
     plans: string[],
     group: string
 }
